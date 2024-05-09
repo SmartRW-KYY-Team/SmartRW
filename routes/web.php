64f33cef03fd169ee31d642912fe64a6f51caf7e
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-05-09 21:57:14 +0700

    fixing bug ci cd

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/nioke', function () {
+Route::get('/nurul', function () {
     return 'hello';
 });
