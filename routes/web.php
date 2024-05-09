0cbabedbf0e7415b01f8d6dc21d182be4a536f27
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-05-09 21:54:11 +0700

    fixing bug ci cd

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
+Route::get('/nioke', function () {
     return 'hello';
 });
