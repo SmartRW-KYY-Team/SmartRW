e64bd09cd82e0a93c056d829876a5b451eed4bb0
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-05-10 01:42:35 +0700

    manage templates layouts

---
+++ resources/views/layouts/pageTitle.blade.php
@@ -0,0 +1,17 @@
+<div class="page-title">
+    <div class="row">
+        <div class="col-12 col-md-6 order-md-1 order-last">
+            <h3>Vertical Layout with Navbar</h3>
+            <p class="text-subtitle text-muted">Navbar will appear on the top of the page.</p>
+        </div>
+        <div class="col-12 col-md-6 order-md-2 order-first">
+            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Layout Vertical Navbar
+                    </li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
