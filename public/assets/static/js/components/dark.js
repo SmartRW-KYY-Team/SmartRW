2871c27bc52a1321719361cb29a2805ea91df614
Cfathurrohman <cfathurrohman@gmail.com>
2024-05-15 21:11:23 +0700

    Delete dark mode

--- public/assets/static/js/components/dark.js
+++ public/assets/static/js/components/dark.js
@@ -26,24 +26,7 @@ function setTheme(theme, persist = false) {
  * Init theme from setTheme()
  */
 function initTheme() {
-    //If the user manually set a theme, we'll load that
-    const storedTheme = localStorage.getItem(THEME_KEY);
-    if (storedTheme) {
-        return setTheme(storedTheme);
-    }
-    //Detect if the user set his preferred color scheme to dark
-    if (!window.matchMedia) {
-        return;
-    }
-
-    //Media query to detect dark preference
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-
-    //Register change listener
-    mediaQuery.addEventListener("change", (e) =>
-        setTheme(e.matches ? "dark" : "light", true)
-    );
-    return setTheme(mediaQuery.matches ? "dark" : "light", true);
+    setTheme("light", true);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
