c5cd2dce9d32c0dfc9a7a763cbba271d403c6c23
fighaz <sofisugiartozaini@gmail.com>
2024-05-15 20:58:19 +0700

    add index kegiatan

---
+++ app/DataTables/KegiatanDataTable.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Kegiatan;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class KegiatanDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', function ($row) {
+                return '<div style="display: flex; justify-content: space-beetween; ">
+                <a href="#" class="btn btn-info me-2"><i class="fa fa-info-circle"></i>Detail</a>
+                <a href="#" class="btn btn-warning me-2"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Ubah</a>
+                    <a href="#" class="btn btn-danger "><i class="fa fa-trash" aria-hidden="true"></i>Hapus</a></div>';
+            })
+            ->addColumn('No', function () {
+                static $index = 1;
+                return $index++;
+            })
+            ->setRowId('id')
+            ->setRowData([
+                'rt' => function (Kegiatan $kegiatan) {
+                    return 'row-' . $kegiatan->rt;
+                },
+                'rw' => function (Kegiatan $kegiatan) {
+                    return 'row-' . $kegiatan->rw;
+                },
+            ]);
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(Kegiatan $model): QueryBuilder
+    {
+        return $model->newQuery()->with(['rt', 'rw']);
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+            ->setTableId('kegiatan-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            //->dom('Bfrtip')
+            ->orderBy(1)
+            ->selectStyleSingle()
+            ->buttons([
+                Button::make('excel'),
+                Button::make('csv'),
+                Button::make('pdf'),
+                Button::make('print'),
+                Button::make('reset'),
+                Button::make('reload')
+            ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('No'),
+            Column::make('nama'),
+            Column::make('tanggal_kegiatan'),
+            Column::make('deskripsi'),
+            Column::make('rt'),
+            Column::make('rw'),
+            Column::computed('action')
+                ->exportable(false)
+                ->printable(false)
+                ->width(60)
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'Kegiatan_' . date('YmdHis');
+    }
+}
