1250eae85c6edc824d6c8d90d9de7f75829a54c8
Cfathurrohman <cfathurrohman@gmail.com>
2024-06-03 10:37:43 +0700

    Update keuangan RT & RW card (saldo, pemasukan, pengeluaran)

--- resources/views/keuanganrw/index.blade.php
+++ resources/views/keuanganrw/index.blade.php
@@ -1,15 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card">
+    <div class="card mb-3">
         <div class="card-header d-flex">
-            <h4 class="card-title">Keuangan RW</h4>
-            <div class="card-tools ms-auto">
-                <a href="{{ url('keuanganrw.create') }}" class="btn btn-md btn-primary mt-1" data-bs-target="#tambahModal" data-bs-toggle="modal">+ Tambah</a>
-            </div>
+            <h4 class="card-title">Keuangan RT</h4>
         </div>
         <div class="card-body">
-            {{ $dataTable->table(['width' => '100%', 'class' => 'table table-bordered table-striped']) }}
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <div class="card bg-white">
+                        <div class="card-header bg-white">
+                            <i class="bi bi-cash fs-2"></i> <span class="fw-bold">Saldo Saat Ini :</span>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Rp {{ number_format($currentBalance, 0, ',', '.') }}</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card bg-white">
+                        <div class="card-header bg-white">
+                            <i class="bi bi-arrow-down-circle-fill text-success fs-2"></i> <span
+                                class="text-success fw-bold">Uang Masuk Bulan Ini :</span>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-success">Rp {{ number_format($monthlyIncome, 0, ',', '.') }}</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card bg-white">
+                        <div class="card-header bg-white">
+                            <i class="bi bi-arrow-up-circle-fill text-danger fs-2"></i> <span
+                                class="text-danger fw-bold">Uang Keluar Bulan Ini :</span>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-danger">Rp {{ number_format($monthlyExpense, 0, ',', '.') }}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-header d-flex">
+                <div class="card-tools ms-auto">
+                    <a href="{{ url('keuangan.create') }}" class="btn btn-md btn-primary mt-1" data-bs-target="#tambahModal"
+                        data-bs-toggle="modal">+ Tambah</a>
+                </div>
+            </div>
+            {{ $dataTable->table(['width' => '100%', 'class' => 'table table-bordered table-striped table-hover']) }}
         </div>
     </div>
     @include('keuanganrw.edit')
@@ -23,7 +60,8 @@
             var id = $(this).data('id');
             $('#deleteModal').modal('show');
             $("#deleteKeuanganModalText").html("Apakah Anda yakin ingin menghapus data ?");
-            $('#deleteForm').attr('action', "{{ route('keuanganrw.destroy', ['id' => 'id_keuanganrw']) }}".replace('id_keuanganrw', id));
+            $('#deleteForm').attr('action', "{{ route('keuanganrw.destroy', ['id' => 'id_keuanganrw']) }}".replace(
+                'id_keuanganrw', id));
         });
 
         $(document).on('click', '.editKeuanganButton', function(e) {
@@ -39,7 +77,9 @@
                     $('#edit-jumlah').val(data.jumlah);
                     $('#edit-rt_id').val(data.rt_id);
 
-                    $('#updateKeuanganForm').attr('action', "{{ route('keuanganrw.update', ['id' => 'id_edit']) }}".replace('id_edit', id_edit));
+                    $('#updateKeuanganForm').attr('action',
+                        "{{ route('keuanganrw.update', ['id' => 'id_edit']) }}".replace('id_edit',
+                            id_edit));
 
                     $('#EditKeuanganModal').modal('show');
                 }
