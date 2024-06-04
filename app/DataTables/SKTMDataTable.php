d2f947563a22766bcdd6f622d1e42624107c72b1
Fyodor-DOs <fathir123t@gmail.com>
2024-06-04 23:50:47 +0700

    upload fitur keuangan, kegiatan

--- app/DataTables/SKTMDataTable.php
+++ app/DataTables/SKTMDataTable.php
@@ -31,8 +31,8 @@ class SKTMDatatable extends DataTable
                 </button>
                 <button type="button" class="btn btn-success me-2 AcceptModalSKTM"
                     data-id="' . $row->id_suratSKTM . '">
-                    <i class="bi bi-check-square-fill"></i> 
-                </button>                
+                    <i class="bi bi-check-square-fill"></i>
+                </button>
             </div>';
             })
             ->addColumn('No', function () {
@@ -103,9 +103,6 @@ class SKTMDatatable extends DataTable
         ];
     }
 
-    /**
-     * Get the filename for export.
-     */
     protected function filename(): string
     {
         return 'Users_' . date('YmdHis');
