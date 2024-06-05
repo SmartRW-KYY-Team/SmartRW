36f69a82d6cc8199b941d51b9f679118f056db2f
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-06-05 15:22:53 +0700

    fixing sktmcontroller

--- app/DataTables/SKTMDataTable.php
+++ app/DataTables/SKTMDataTable.php
@@ -12,7 +12,7 @@ use Yajra\DataTables\Html\Editor\Editor;
 use Yajra\DataTables\Html\Editor\Fields;
 use Yajra\DataTables\Services\DataTable;
 
-class SKTMDatatable extends DataTable
+class SKTMDataTable extends DataTable
 {
     /**
      * Build the DataTable class.
