7c99d2b1306dd2e27085396bb3166a4bcfc7d337
Bagus Arnovario Wibowo <variosrio@gmail.com>
2024-06-05 22:23:55 +0700

    fix dashboard

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -4,10 +4,9 @@
     <div class="container">
         <div class="card">
             <div class="card-header">
-                <h1>Dashboard</h1>
             </div>
             <div class="card-body">
-                <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/53f55aea-ecea-4764-9e84-7d797eb660da/page/p_g7fl365yhd" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+                <iframe width="1020" height="800" src="https://lookerstudio.google.com/embed/reporting/53f55aea-ecea-4764-9e84-7d797eb660da/page/p_g7fl365yhd" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
             </div>
         </div>
     </div>
