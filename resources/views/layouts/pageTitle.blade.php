098f6adf51a5c1a5169f74d67c2315fc43a78eab
Cfathurrohman <cfathurrohman@gmail.com>
2024-06-12 00:37:38 +0700

    Update breadcrumb

--- resources/views/layouts/pageTitle.blade.php
+++ resources/views/layouts/pageTitle.blade.php
@@ -7,7 +7,7 @@
         <div class="col-12 col-md-6 order-md-2 order-first">
             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">{{ $pageTitle }}</a></li>
+                    <li class="breadcrumb-item"><p href="index.html">{{ $pageTitle }}</p></li>
                     <li class="breadcrumb-item active" aria-current="page">{{ $activePosition }}
                     </li>
                 </ol>
