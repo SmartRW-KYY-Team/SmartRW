626807439727102fbd3e063eb2bd5abcfeaab87a
Fyodor-DOs <fathir123t@gmail.com>
2024-06-14 02:02:06 +0700

    upload landing

--- resources/views/layouts_landing/header.blade.php
+++ resources/views/layouts_landing/header.blade.php
@@ -8,8 +8,8 @@
 
         <nav id="navmenu" class="navmenu">
             <ul>
-                <li><a href="#hero" class="active">Beranda<br></a></li>
-                <li><a href="{{ route('pengaduan_page') }}">Pengaduan</a></li>
+                <li><a href="{{ route('landing_page') }}" class="{{ Request::is('/*') ? 'active' : '' }}">Beranda</a></li>
+                <li><a href="{{ route('pengaduan_page') }}" class="{{ Request::is('pengaduan_page*') ? 'active' : '' }}">Pengaduan</a></li>
                 <li class="dropdown"><a href="#"><span>Keuangan</span> <i
                             class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
@@ -30,17 +30,17 @@
                             class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
 
-                        <li class="dropdown"><a href="{{ route('sktm_page') }}"><span>Surat SKTM</span> <i
+                        <li class="dropdown"><a href="{{ route('sktm_page') }}" class="{{ Request::is('sktm_page*') ? 'active' : '' }}"><span>Surat SKTM</span> <i
                                     class="bi bi-chevron-down toggle-dropdown"></i></a>
                             <ul>
-                                <li><a href="{{ route('cek_sktm_page') }}">Status SKTM</a></li>
+                                <li><a href="{{ route('cek_sktm_page') }}" class="{{ Request::is('cek_sktm_page*') ? 'active' : '' }}">Status SKTM</a></li>
 
                             </ul>
                         </li>
-                        <li class="dropdown"><a href="{{ route('domisili_page') }}"><span>Surat Domisili</span> <i
+                        <li class="dropdown"><a href="{{ route('domisili_page') }}" class="{{ Request::is('domisili_page*') ? 'active' : '' }}"><span>Surat Domisili</span> <i
                                     class="bi bi-chevron-down toggle-dropdown"></i></a>
                             <ul>
-                                <li><a href="{{ route('cek_domisili_page') }}">Status Domisili</a></li>
+                                <li><a href="{{ route('cek_domisili_page') }}" class="{{ Request::is('cek_domisili_page*') ? 'active' : '' }}">Status Domisili</a></li>
 
                             </ul>
                         </li>
@@ -51,7 +51,7 @@
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
 
-        <a class="btn-getstarted flex-md-shrink-0" href="index.html#about">Login</a>
+        <a class="btn-getstarted flex-md-shrink-0" href="{{ route('login') }}">Login</a>
 
     </div>
 </header>
