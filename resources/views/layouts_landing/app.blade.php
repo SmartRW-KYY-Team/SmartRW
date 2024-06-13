35f92fc10f439cc3f5a94eae7b386f88e4889410
Cfathurrohman <cfathurrohman@gmail.com>
2024-06-13 21:24:28 +0700

    Update ladning page contact

--- resources/views/layouts_landing/app.blade.php
+++ resources/views/layouts_landing/app.blade.php
@@ -1294,96 +1294,50 @@
             <!-- Section Title -->
             <div class="container section-title" data-aos="fade-up">
                 <h2>Contact</h2>
-                <p>Contact Us</p>
+                <p>Hubungi Kami</p>
             </div><!-- End Section Title -->
 
             <div class="container" data-aos="fade-up" data-aos-delay="100">
-
                 <div class="row gy-4">
-
-                    <div class="col-lg-6">
-
+                    <div class="col-lg-12">
                         <div class="row gy-4">
-                            <div class="col-md-6">
+                            <div class="col-md-3">
                                 <div class="info-item" data-aos="fade" data-aos-delay="200">
                                     <i class="bi bi-geo-alt"></i>
-                                    <h3>Address</h3>
-                                    <p>A108 Adam Street</p>
-                                    <p>New York, NY 535022</p>
+                                    <h3>Alamat</h3>
+                                    <p>Jl. Kembang Turi, Lowokwaru</p>
+                                    <p>Kota Malang</p>
                                 </div>
                             </div><!-- End Info Item -->
-
-                            <div class="col-md-6">
+                            <div class="col-md-3">
                                 <div class="info-item" data-aos="fade" data-aos-delay="300">
                                     <i class="bi bi-telephone"></i>
                                     <h3>Call Us</h3>
-                                    <p>+1 5589 55488 55</p>
-                                    <p>+1 6678 254445 41</p>
+                                    <a href="https://wa.me/628110992160" style="color: #5d6df3; text-decoration: underline;">+6282110992160</a>
+                                    <br><br>
                                 </div>
                             </div><!-- End Info Item -->
-
-                            <div class="col-md-6">
+                            <div class="col-md-3">
                                 <div class="info-item" data-aos="fade" data-aos-delay="400">
                                     <i class="bi bi-envelope"></i>
                                     <h3>Email Us</h3>
-                                    <p>info@example.com</p>
-                                    <p>contact@example.com</p>
+                                    <p>info@smartrw.co.id</p>
+                                    <br>
                                 </div>
                             </div><!-- End Info Item -->
-
-                            <div class="col-md-6">
+                            <div class="col-md-3">
                                 <div class="info-item" data-aos="fade" data-aos-delay="500">
                                     <i class="bi bi-clock"></i>
-                                    <h3>Open Hours</h3>
-                                    <p>Monday - Friday</p>
-                                    <p>9:00AM - 05:00PM</p>
+                                    <h3>Jam Respon</h3>
+                                    <p>Senin - Jumat</p>
+                                    <p>07.00 WIB - 15.00 WIB</p>
                                 </div>
                             </div><!-- End Info Item -->
-
                         </div>
-
                     </div>
-
-                    <div class="col-lg-6">
-                        <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up"
-                            data-aos-delay="200">
-                            <div class="row gy-4">
-
-                                <div class="col-md-6">
-                                    <input type="text" name="name" class="form-control"
-                                        placeholder="Your Name" required="">
-                                </div>
-
-                                <div class="col-md-6 ">
-                                    <input type="email" class="form-control" name="email"
-                                        placeholder="Your Email" required="">
-                                </div>
-
-                                <div class="col-md-12">
-                                    <input type="text" class="form-control" name="subject" placeholder="Subject"
-                                        required="">
-                                </div>
-
-                                <div class="col-md-12">
-                                    <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
-                                </div>
-
-                                <div class="col-md-12 text-center">
-                                    <div class="loading">Loading</div>
-                                    <div class="error-message"></div>
-                                    <div class="sent-message">Your message has been sent. Thank you!</div>
-
-                                    <button type="submit">Send Message</button>
-                                </div>
-
-                            </div>
-                        </form>
-                    </div><!-- End Contact Form -->
-
                 </div>
-
             </div>
-
+            
         </section><!-- /Contact Section -->
 
     </main>
