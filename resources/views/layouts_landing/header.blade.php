8079f647f5ac30a738a1c3aef463ee2ad5aea7f8
Fyodor-DOs <fathir123t@gmail.com>
2024-06-14 03:39:42 +0700

    minor fix

--- resources/views/layouts_landing/header.blade.php
+++ resources/views/layouts_landing/header.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="header d-flex align-items-center fixed-top">
     <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
-        <a href="index.html" class="logo d-flex align-items-center me-auto">
+        <a href="{{ route('landing_page') }}" class="logo d-flex align-items-center me-auto">
             <!-- Uncomment the line below if you also wish to use an image logo -->
             <img src="{{ asset('assets/image/logo-navbar.svg') }}" alt="SmartRW">
         </a>
